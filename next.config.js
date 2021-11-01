adb70aab27859ce40e09bdb5354109e5689fc081
pancake <834430775@qq.com>
2021-11-01 23:02:45 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
+const {plugin} = require('next/dist/build/webpack/config/helpers')
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.png$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options:{
+            outputPath: 'static'
+          }
+        }
+      ]
+    })
+
+    return config
+  }
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import 'styles/globals.css'
+import Head from 'next/head'
+import React from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>我的博客 - Pank</title>
+      <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
