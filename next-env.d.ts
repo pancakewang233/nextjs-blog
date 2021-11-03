dc6dd055ebfe4e7874f1a75ce0282d2223ebacd9
pancake <834430775@qq.com>
2021-11-03 22:08:21 +0800

    后端静态渲染ssg

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,3 +4,9 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+type Post = {
+    id: string;
+    date: string;
+    title: string
+}
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,19 +1,36 @@
 import { usePosts } from "hooks/usePosts";
-import { NextPage } from "next";
+import getPosts from "lib/posts";
+import { GetStaticProps, NextPage } from "next";
 
-const PostsIndex:NextPage = () => {
-    const {isLoading,isEmpty,posts} = usePosts()
+type Props = {
+    posts: Post[]
+}
+const PostsIndex:NextPage<Props> = (props) => {
+    // const {isLoading,isEmpty,posts} = usePosts()
+    const {posts} = props
     return (
         <div>
             <h1>文章列表</h1>
-            {isLoading ? <div>加载中</div> :
+            {/* {isLoading ? <div>加载中</div> :
                 isEmpty ? <div>没用文章</div> :
                 posts.map(p=><div key={p.id}>
                     {p.id}
                 </div>)
-            }
+            } */}
+            {posts.map(p=><div key={p.id}>
+                {p.id}
+            </div>)}
         </div>
     )
 }
 
 export default PostsIndex;
+
+export const getStaticProps:GetStaticProps = async() =>{
+    const posts = await getPosts()
+    return {
+        props: {
+            posts: posts
+        }
+    }
+}
